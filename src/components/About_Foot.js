fad5d42bf47e102c3f08fc0134235666899a6d98
Manikandan5398 <manigandan5398@gmail.com>
2025-03-15 19:41:31 +0530

    updated phone

--- src/components/About_Foot.js
+++ src/components/About_Foot.js
@@ -13,7 +13,7 @@ function About(){
     return (        <footer>
       <div className="contact">
           <h3>Contact</h3>
-          <p style={{marginTop: "5px"}}>+91 3478631928 <br></br>
+          <p style={{marginTop: "5px"}}>+91 7200889299 <br></br>
               perfumy@gmail.com
           </p>
   
@@ -22,7 +22,7 @@ function About(){
       W3Schools is optimized for learning and training. Examples might be simplified to improve reading and learning. Tutorials, references, and examples are constantly reviewed to avoid errors, but we cannot warrant full correctness of all content. While using W3Schools, you agree to have read and accepted our terms of use, cookie and privacy policy.
   
       Copyright 1999-2023 by Refsnes Data. All Rights Reserved.
-      W3Schools is Powered by W3.CSS.
+     Manikandan Vaishu
      </p>
   </footer>)
   }
